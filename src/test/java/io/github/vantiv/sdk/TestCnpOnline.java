426f3f65bc92a2aeaa4ee663877547787fee6363
Shivraj Tapkir <stapkir@nufl2rdd00431.vapdev.int>
2024-04-25 09:43:58 -0400

    SDK changes for Java17

--- src/test/java/io/github/vantiv/sdk/TestCnpOnline.java
+++ src/test/java/io/github/vantiv/sdk/TestCnpOnline.java
@@ -7,6 +7,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.xml.bind.JAXBElement;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import java.math.BigInteger;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -17,6 +20,7 @@ import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.matches;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.when;
 
 public class TestCnpOnline {
@@ -1526,7 +1530,8 @@ public class TestCnpOnline {
         activateReversal.setOriginalRefCode("3");
         activateReversal.setOriginalSequenceNumber("999999");
         activateReversal.setOriginalSystemTraceId(5);
-        activateReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        activateReversal.setOriginalTxnTime(timestamp);
 
         Communication mockedCommunication = mock(Communication.class);
         when(
@@ -1597,7 +1602,8 @@ public class TestCnpOnline {
         deactivateReversal.setOriginalRefCode("3");
         deactivateReversal.setOriginalSequenceNumber("999999");
         deactivateReversal.setOriginalSystemTraceId(5);
-        deactivateReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        deactivateReversal.setOriginalTxnTime(timestamp);
 
         Communication mockedCommunication = mock(Communication.class);
         when(
@@ -1669,7 +1675,8 @@ public class TestCnpOnline {
         loadReversal.setOriginalRefCode("3");
         loadReversal.setOriginalSequenceNumber("999999");
         loadReversal.setOriginalSystemTraceId(5);
-        loadReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        loadReversal.setOriginalTxnTime(timestamp);
 
         Communication mockedCommunication = mock(Communication.class);
         when(
@@ -1741,7 +1748,8 @@ public class TestCnpOnline {
         unloadReversal.setOriginalRefCode("3");
         unloadReversal.setOriginalSequenceNumber("999999");
         unloadReversal.setOriginalSystemTraceId(5);
-        unloadReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        unloadReversal.setOriginalTxnTime(timestamp);
 
         Communication mockedCommunication = mock(Communication.class);
         when(
@@ -1814,7 +1822,8 @@ public class TestCnpOnline {
         refundReversal.setCard(giftCard);
         refundReversal.setOriginalRefCode("ref");
         refundReversal.setOriginalAmount(44455L);
-        refundReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        refundReversal.setOriginalTxnTime(timestamp);
         refundReversal.setOriginalSystemTraceId(3);
         refundReversal.setOriginalSequenceNumber("222222");
         
@@ -1889,7 +1898,8 @@ public class TestCnpOnline {
         depositReversal.setCard(giftCard);
         depositReversal.setOriginalRefCode("101");
         depositReversal.setOriginalAmount(3456L);
-        depositReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        depositReversal.setOriginalTxnTime(timestamp);
         depositReversal.setOriginalSystemTraceId(33);
         depositReversal.setOriginalSequenceNumber("111111");
         
@@ -1964,7 +1974,8 @@ public class TestCnpOnline {
         gcAuthReversal.setCard(giftCard);
         gcAuthReversal.setOriginalRefCode("ref");
         gcAuthReversal.setOriginalAmount(44455L);
-        gcAuthReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        gcAuthReversal.setOriginalTxnTime(timestamp);
         gcAuthReversal.setOriginalSystemTraceId(3);
         gcAuthReversal.setOriginalSequenceNumber("222222");
         
@@ -2026,7 +2037,8 @@ public class TestCnpOnline {
         gcAuthReversal.setCard(giftCard);
         gcAuthReversal.setOriginalRefCode("ref");
         gcAuthReversal.setOriginalAmount(44455L);
-        gcAuthReversal.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        gcAuthReversal.setOriginalTxnTime(timestamp);
         gcAuthReversal.setOriginalSystemTraceId(3);
         gcAuthReversal.setOriginalSequenceNumber("222222");
         
@@ -2092,7 +2104,8 @@ public class TestCnpOnline {
         gcCapture.setCard(giftCard);
         gcCapture.setOriginalRefCode("ref");
         gcCapture.setOriginalAmount(44455L);
-        gcCapture.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        gcCapture.setOriginalTxnTime(timestamp);
         
         Communication mockedCommunication = mock(Communication.class);
         when(
@@ -2152,7 +2165,8 @@ public class TestCnpOnline {
         gcCapture.setCard(giftCard);
         gcCapture.setOriginalRefCode("ref");
         gcCapture.setOriginalAmount(44455L);
-        gcCapture.setOriginalTxnTime(new XMLGregorianCalendarImpl());
+		XMLGregorianCalendar timestamp= DatatypeFactory.newInstance().newXMLGregorianCalendar();
+        gcCapture.setOriginalTxnTime(timestamp);
         
         Communication mockedCommunication = mock(Communication.class);
         when(
